1dd3e3673325984b9e64e2b7b0c761d676a5d23d
Mamoru Goto <mamoru.goto@frisys.jp>
2026-08-04 17:33:02 +0900

    vercel buildエラー解消

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   devIndicators: false,
-  env: {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    ...require(`./config/${process.env.APP_ENV || 'local'}.json`),
-  },
 }
 
 export default nextConfig;
